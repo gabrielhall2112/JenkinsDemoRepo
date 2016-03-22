c790a6d5b1e337d0db415baaf6302d48043ae7da
Gabriel Hall <gabriel.hall@research.ucla.edu>
2016-03-22 09:48:06 -0700

    space

--- JenkinsDemo/JenkinsDemo/Controllers/HomeController.cs
+++ JenkinsDemo/JenkinsDemo/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace JenkinsDemo.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page. ";
 
             return View();
         }
